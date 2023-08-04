086fb7e0dc6d5257ab4576ea152401b218a511e0
ammoru <119839681+ammoru@users.noreply.github.com>
2023-08-04 21:16:52 +0530

    import  java.util.Scanner;
    public class Sample{
        public static void main(String [] argv)
        {
            Scanner sc = new Scanner(System.in);
            String str=sc.nextLine();
            System.out.println(str);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import  java.util.Scanner;
+
+public class Sample{
+    public static void main(String [] argv)
+    {
+        Scanner sc = new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+}
